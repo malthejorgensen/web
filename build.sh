05ad6b5bc864c2f028d9c422c629bdae3d3813d0
Malthe Jørgensen <malthe.banan@gmail.com>
2013-03-19 20:35:10 +0100

    Add chaos_org_chart.tex

--- build.sh
+++ build.sh
@@ -2,13 +2,19 @@
 # http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 command -v luatex >/dev/null 2>&1 || { echo >&2 "I require luatex (lualatex) but it's not installed.  Aborting."; exit 1; }
 
-renderTemplateCheetah chaos_objects.tex >| chaos_objects.cheetah.tex
+# Create temporary work directory
 mkdir tex-temp
-lualatex --output-directory tex-temp --shell-escape --jobname chaos_objects chaos_objects.cheetah.tex
+
+renderTemplateCheetah chaos_objects.tex >| tex-temp/chaos_objects.cheetah.tex
+lualatex --output-directory tex-temp --shell-escape --jobname chaos_objects tex-temp/chaos_objects.cheetah.tex
+
+renderTemplateCheetah chaos_org_chart.tex >| tex-temp/chaos_org_chart.cheetah.tex
+lualatex --output-directory tex-temp --shell-escape --jobname chaos_org_chart tex-temp/chaos_org_chart.cheetah.tex
 
 
 if command -v pdftoppm >/dev/null 2>&1; then
   pdftoppm -png tex-temp/chaos_objects.pdf > chaos_objects.png;
+  pdftoppm -png tex-temp/chaos_org_chart.pdf > chaos_org_chart.png;
 else
   echo >&2 "";
   echo >&2 "-------------------------------------------------------------";
@@ -16,5 +22,4 @@ else
 fi
 
 # Cleanup
-rm chaos_objects.cheetah.tex
 rm -rf tex-temp
